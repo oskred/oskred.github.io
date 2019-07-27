cb9dce8d5ec610bcd3632d2160327bc7eb743e06
Oscar Abella <oabella@fusionapps.com>
2019-07-27 19:08:07 -0400

    updated font awesome lib, fixed my expertise

--- logic.js
+++ logic.js
@@ -11,7 +11,9 @@ function doScrolling(element, duration) {
   var diff = targetY - startingY
   // Easing function: easeInOutCubic
   // From: https://gist.github.com/gre/1650294
-  var easing = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 }
+  var easing = function(t) {
+    return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+  }
   var start
 
   if (!diff) return
@@ -37,33 +39,47 @@ function doScrolling(element, duration) {
 }
 
 // Apply event handlers. Example of firing the scrolling mechanism.
-document.getElementById('scrollMyExpertise').addEventListener('click', doScrolling.bind(null, '#myExpertise', 1000))
-document.getElementById('scrollMyExpertise1').addEventListener('click', doScrolling.bind(null, '#myExpertise', 1000))
+// document.getElementById('scrollMyExpertise').addEventListener('click', doScrolling.bind(null, '#myExpertise', 1000))
+// document.getElementById('scrollMyExpertise1').addEventListener('click', doScrolling.bind(null, '#myExpertise', 1000))
 
 
 // hover zoom enabler
 $('.contentFiveA')
-    // tile mouse actions
-    .on('mouseover', function(){
-      $(this).children('.photo').css({'transform': 'scale('+ 
-      $(this).attr('data-scale') +')'});
-    })
-    .on('mouseout', function(){
-      $(this).children('.photo').css({'transform': 'scale(1)'});
-    })
-    .on('mousemove', function(e){
-          $(this).children('.photo').css({'transform-origin': 
-        ((e.pageX - $(this).offset().left) / $(this).width()) * 
-        100 + '% ' + ((e.pageY - $(this).offset().top) / 
-        $(this).height()) * 100 +'%'});
-    })
-    // tiles set up
-    .each(function(){
-      $(this)
-        // add a photo container
-        .append('<div class="photo"></div>')
-        // some text just to show zoom level on current item in this example
-        .append('<div class="txt"><div class="x">Want to work together?</div><br> I' + "'" + 'm always open to new ideas. Please feel free to contact me and let me know a little about the opportunity.  <br><br> <a href="mailto:abella.eduardo@gmail.com"><button type="button" style="border-color: #ff8c00; border-radius: 50px; border-width: 2.5px;" class="btn btn-zoom btn-lg btn-primary-outline">Contact me</button></a></div>')
-        // set up a background image for each tile based on data-image attribute
-        .children('.photo').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
-    })
+  // tile mouse actions
+  .on('mouseover', function() {
+    $(this).children('.photo').css({
+      'transform': 'scale(' +
+        $(this).attr('data-scale') + ')'
+    });
+  })
+  .on('mouseout', function() {
+    $(this).children('.photo').css({
+      'transform': 'scale(1)'
+    });
+  })
+  .on('mousemove', function(e) {
+    $(this).children('.photo').css({
+      'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) *
+        100 + '% ' + ((e.pageY - $(this).offset().top) /
+          $(this).height()) * 100 + '%'
+    });
+  })
+  // tiles set up
+  .each(function() {
+    $(this)
+      // add a photo container
+      .append('<div class="photo"></div>')
+      // some text just to show zoom level on current item in this example
+      .append('<div class="txt"><div class="x">Want to work together?</div><br> I' + "'" + 'm always open to new ideas. Please feel free to contact me and let me know a little about the opportunity.  <br><br> <a href="mailto:abella.eduardo@gmail.com"><button type="button" style="border-color: #ff8c00; border-radius: 50px; border-width: 2.5px;" class="btn btn-zoom btn-lg btn-primary-outline">Contact me</button></a></div>')
+      // set up a background image for each tile based on data-image attribute
+      .children('.photo').css({
+        'background-image': 'url(' + $(this).attr('data-image') + ')'
+      });
+  })
+
+$(function() {
+  $(document).scroll(function() {
+    var $nav = $(".main-nav");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
